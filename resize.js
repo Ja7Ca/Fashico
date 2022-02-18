6a0d2564e1253d060888118da80730a543473a80
Ja7Ca <76737729+Ja7Ca@users.noreply.github.com>
2022-02-18 11:37:07 +0700

    Add files via upload

---
+++ resize.js
@@ -0,0 +1,9 @@
+$(window).on('resize', function(){
+    var win = $(this); 
+    if (win.width() <= 1280) { 
+        // $(".move-scroll-down").css({
+        //     opacity: "0",
+        //     transiton: "3s",
+        // })
+     }
+})
--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function showNavigator(){
     if(nav.style.opacity == 0){
         nav.style.opacity = 100;
         nav.style.transitionDuration = "0.5s";
-        nav.translateZ = "2px";
+        // nav.translateZ = "2px";
     } else {
         nav.style.opacity = 00;
         nav.style.transitionDuration = "0.5s";
-        nav.translateZ = "-2px";
+        // nav.translateZ = "-2px";
     }
 }
